5d607785c54e59a14b4cd8e1c5e7708c79f782fb
Stefan Radu <stefan270101@gmail.com>
2023-01-07 19:20:03 +0200

    tested motor turning

--- line-follower.ino
+++ line-follower.ino
@@ -1,58 +1,36 @@
-//L293D
-//Motor A
-const int motorPin1  = 5;  // Pin 14 of L293
-const int motorPin2  = 6;  // Pin 10 of L293
-//Motor B
-const int motorPin3  = 10; // Pin  7 of L293
-const int motorPin4  = 9;  // Pin  2 of L293
+#include "MotorController.h"
+#include "QTRSensorController.h"
+#include "Global.h"
+
+const motor leftMotor {LEFT_MOTOR_PLUS_PIN, LEFT_MOTOR_MINUS_PIN};
+const motor rightMotor {RIGHT_MOTOR_PLUS_PIN, RIGHT_MOTOR_MINUS_PIN};
 
 //This will run only one time.
 void setup(){
- 
-    //Set pins as outputs
-    pinMode(motorPin1, OUTPUT);
-    pinMode(motorPin2, OUTPUT);
-    pinMode(motorPin3, OUTPUT);
-    pinMode(motorPin4, OUTPUT);
-    
-    //Motor Control - Motor A: motorPin1,motorpin2 & Motor B: motorpin3,motorpin4
+  Serial.begin(9600);
 
-    //This code  will turn Motor A clockwise for 2 sec.
-    analogWrite(motorPin1, 150);
-    digitalWrite(motorPin2, LOW);
-    digitalWrite(motorPin3, LOW);
-    digitalWrite(motorPin4, LOW);
-    delay(2000); 
-    //This code will turn Motor A counter-clockwise for 2 sec.
-    digitalWrite(motorPin1, LOW);
-    digitalWrite(motorPin2, HIGH);
-    digitalWrite(motorPin3, LOW);
-    digitalWrite(motorPin4, LOW);
-    delay(2000);
-    
-    //This code will turn Motor B clockwise for 2 sec.
-    digitalWrite(motorPin1, LOW);
-    digitalWrite(motorPin2, LOW);
-    digitalWrite(motorPin3, HIGH);
-    digitalWrite(motorPin4, LOW);
-    delay(2000); 
-    //This code will turn Motor B counter-clockwise for 2 sec.
-    digitalWrite(motorPin1, LOW);
-    digitalWrite(motorPin2, LOW);
-    digitalWrite(motorPin3, LOW);
-    digitalWrite(motorPin4, HIGH);
-    delay(2000);    
-    
-    //And this code will stop motors
-    digitalWrite(motorPin1, LOW);
-    digitalWrite(motorPin2, LOW);
-    digitalWrite(motorPin3, LOW);
-    digitalWrite(motorPin4, LOW);
-  
+//  initMotor(leftMotor);
+//  initMotor(rightMotor);
+//  
+//  runMotor(leftMotor, true, 200);
+//  runMotor(rightMotor, true, 200);
+//  
+//  delay(3000);
+//  
+//  stopMotor(rightMotor);
+//  
+//  delay(1000);
+//  
+//  runMotor(rightMotor, false, 255);
+//  
+//  stopMotor(leftMotor);
+//  stopMotor(rightMotor);
+
+  qtrInit();
+  qtrCalibrate();
 }
 
 
 void loop(){
-  
-
+   qtrGetBlackLinePosition(true);
 }
